f1cc3d0c7d6183ee353ce46f40f23b7159f3e8cf
ramohin1236 <mohinr26@gmail.com>
2026-02-15 17:01:26 +0600

    icon color

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -42,10 +42,10 @@ function Login() {
               <button
                 type="button"
                 onClick={() => setShowPassword((show) => !show)}
-                className="absolute right-3 top-1/2 -translate-y-1/2 flex items-center justify-center p-1 text-gray-600"
+                className="absolute right-3 top-1/3 -translate-y-1/2 flex items-center justify-center p-1 text-gray-600"
                 aria-label={showPassword ? "Hide password" : "Show password"}
               >
-                {showPassword ? (<EyeOff className="w-5 h-5" /> ) : ( <Eye className="w-5 h-5" /> )}
+                {showPassword ? (<EyeOff className="w-5 h-5 text-[#B0B0B0]" /> ) : ( <Eye className="w-5 h-5 text-[#B0B0B0]" /> )}
                   
                 
               </button>
