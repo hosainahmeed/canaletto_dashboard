de3442c70155bd27dc8d8f093e3eb5859d08f1f0
ramohin1236 <mohinr26@gmail.com>
2026-02-16 10:39:13 +0600

    otp resend

--- src/pages/ForgotOtp.tsx
+++ src/pages/ForgotOtp.tsx
@@ -5,7 +5,6 @@ import { RedirectByRole } from "../lib/redirects";
 import { setUserInLocalStorage } from "../lib/auth";
 import { Input } from "../components/ui/input";
 
-
 function ForgotPassword() {
   const navigate = useNavigate();
   const location = useLocation();
@@ -13,34 +12,43 @@ function ForgotPassword() {
   const { email } = location.state || { email: "" };
 
   const [code, setCode] = useState<string[]>(["", "", "", "", "", ""]);
-
   const [loading, setLoading] = useState(false);
+  const [resending, setResending] = useState(false);
   const [error, setError] = useState<string | null>(null);
+  const [successMessage, setSuccessMessage] = useState<string | null>(null);
 
-//   otp input number 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>, index:number) => {
-     const newCodee = [...code];
-     newCodee[index] = e.target.value;
-     setCode(newCodee);
+  // otp input
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    index: number,
+  ) => {
+    const newCodee = [...code];
+    newCodee[index] = e.target.value;
+    setCode(newCodee);
 
-     if (e.target.value && index < 6 - 1) {
-      const nextInput = document.getElementById(`input-${index + 1}`) as HTMLInputElement;
+    if (e.target.value && index < 6 - 1) {
+      const nextInput = document.getElementById(
+        `input-${index + 1}`,
+      ) as HTMLInputElement;
       nextInput?.focus();
     }
   };
 
-// otp remove with backspace
-  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>,index: number) => {
- 
+  // otp remove
+  const handleKeyDown = (
+    e: React.KeyboardEvent<HTMLInputElement>,
+    index: number,
+  ) => {
     if (e.key === "Backspace" && !code[index]) {
-      const prevInput = document.getElementById(`input-${index - 1}`) as HTMLInputElement;
+      const prevInput = document.getElementById(
+        `input-${index - 1}`,
+      ) as HTMLInputElement;
       prevInput?.focus();
     }
   };
 
-
-// otp submit 
-   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+  // otp submit
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     setError(null);
 
@@ -51,20 +59,36 @@ function ForgotPassword() {
       return;
     }
 
-    try{
-        setLoading(true);
-        await new Promise((resolve) => setTimeout(resolve, 1000));
-        const user = setUserInLocalStorage(email);
-        RedirectByRole(user.role, navigate);
-    
+    try {
+      setLoading(true);
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      const user = setUserInLocalStorage(email);
+      RedirectByRole(user.role, navigate);
     } catch (err: any) {
-        setError(err.message || "Something went wrong. Please try again.");
+      setError(err.message || "Something went wrong. Please try again.");
+    } finally {
+      setLoading(false);
     }
-      finally{
-         setLoading(false);
-      } 
-   }
-    
+  };
+
+  // otp resend
+  const handleResend = async () => {
+    setResending(true);
+    setError(null);
+    setSuccessMessage(null);
+
+    try {
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+
+      console.log(`Verification code resent to ${email}`);
+
+      setSuccessMessage("A new code has been sent to your email!");
+    } catch (err: any) {
+      setError("Something went wrong while resending the code.");
+    } finally {
+      setResending(false);
+    }
+  };
 
   return (
     <div className="min-h-screen bg-[#F9F9F9] flex items-center justify-center px-4">
@@ -74,15 +98,13 @@ function ForgotPassword() {
             Check your email
           </h2>
           <p className="text-sm sm:text-base text-[#B0B0B0]">
-            We sent a reset link to {email} enter 6 digit code that mentioned in the email
+            We sent a reset link to {email} enter 6 digit code that mentioned in
+            the email
           </p>
-         
         </div>
 
-        <form 
-        onSubmit={handleSubmit} 
-        className="flex flex-col gap-4">
-           <div className="flex justify-between items-center gap-3">
+        <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+          <div className="flex justify-between items-center gap-3">
             {code.map((digit, index) => (
               <Input
                 key={index}
@@ -97,17 +119,22 @@ function ForgotPassword() {
             ))}
           </div>
 
-
-         {error && (
+          {error && (
             <div className="text-sm text-red-500 bg-red-50 p-2 rounded-md mt-2">
               {error}
             </div>
           )}
-          
+
+          {successMessage && (
+            <div className="text-sm text-green-500 bg-green-50 p-2 rounded-md mt-2">
+              {successMessage}
+            </div>
+          )}
+
           <button
             type="submit"
             disabled={loading}
-            className="w-full py-3 rounded-xl bg-linear-to-r from-[#D4B785] to-[#B08D59] text-white font-medium flex items-center justify-center gap-2 disabled:opacity-70 mt-6" 
+            className="w-full py-3 rounded-xl bg-linear-to-r from-[#D4B785] to-[#B08D59] text-white font-medium flex items-center justify-center gap-2 disabled:opacity-70 mt-6"
           >
             {loading ? (
               <>
@@ -119,6 +146,23 @@ function ForgotPassword() {
             )}
           </button>
         </form>
+
+        <div className="mt-4">
+          <p className="text-sm sm:text-base text-center text-[#B0B0B0]">
+            You have not received the email?
+            <button
+              className="ml-2 text-[#4682b9] font-medium cursor-pointer underline"
+              onClick={handleResend}
+              disabled={resending}
+            >
+              {resending ? (
+                <Loader className="animate-spin w-4 h-4" />
+              ) : (
+                "Resend"
+              )}
+            </button>
+          </p>
+        </div>
       </div>
     </div>
   );
